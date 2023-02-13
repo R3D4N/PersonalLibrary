9acdeb1ff372b4a4302cb28f75e814dd38de82b1
R3_D4N <bellidoangelo97@gmail.com>
2023-02-13 00:10:54 -0500

    Completed get and post request /api/books

--- routes/api.js
+++ routes/api.js
@@ -7,18 +7,36 @@
 */
 
 'use strict';
-
+const Book = require('../models/book.js');
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      Book.find({}, (err, booksData)=>{
+        if(err) return res.json({error: err})
+        if(!booksData) return res.json({msj: 'no books in data base'})
+        let bookJSON =[]
+        booksData.forEach(book => {
+          bookJSON.push({
+            '_id': book._id,
+            'title': book.title,
+            'commentcount': book.comments.length
+          })
+        })
+        res.json(bookJSON)
+      })
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if(!title) return res.json({msj: 'missing required field title'})
+      Book.create({title}, (err, bookData)=>{
+        if(err) return res.json({error: err})
+        res.json(bookData)
+      })
     })
     
     .delete(function(req, res){
